554de3597fa5cab5bc511a94a686ac127ac0d458
Vatler <Vatler@LAPTOP-FSCGGG42>
2024-04-11 15:23:41 +0200

    added extra obstacle

--- SL2aGame/Form1.Designer.cs
+++ SL2aGame/Form1.Designer.cs
@@ -29,37 +29,19 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.Floor = new System.Windows.Forms.PictureBox();
-            this.Avatar = new System.Windows.Forms.PictureBox();
             this.scoreText = new System.Windows.Forms.Label();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Floor = new System.Windows.Forms.PictureBox();
+            this.Avatar = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Floor)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Avatar)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
-            // 
-            // Floor
-            // 
-            this.Floor.BackColor = System.Drawing.Color.Black;
-            this.Floor.Location = new System.Drawing.Point(-12, 452);
-            this.Floor.Name = "Floor";
-            this.Floor.Size = new System.Drawing.Size(652, 23);
-            this.Floor.TabIndex = 0;
-            this.Floor.TabStop = false;
-            // 
-            // Avatar
-            // 
-            this.Avatar.BackColor = System.Drawing.Color.Transparent;
-            this.Avatar.Image = global::SL2aGame.Properties.Resources.Avatar;
-            this.Avatar.Location = new System.Drawing.Point(46, 405);
-            this.Avatar.Name = "Avatar";
-            this.Avatar.Size = new System.Drawing.Size(24, 48);
-            this.Avatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.Avatar.TabIndex = 1;
-            this.Avatar.TabStop = false;
             // 
             // scoreText
             // 
@@ -77,20 +59,8 @@
             this.gameTimer.Interval = 20;
             this.gameTimer.Tick += new System.EventHandler(this.gameEvent);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::SL2aGame.Properties.Resources.obstacle_hydrant;
-            this.pictureBox1.Location = new System.Drawing.Point(261, 428);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(16, 24);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Tag = "obstacle";
-            // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = global::SL2aGame.Properties.Resources.obstacle_berrybush;
             this.pictureBox2.Location = new System.Drawing.Point(421, 428);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(16, 24);
@@ -99,11 +69,52 @@
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Tag = "obstacle";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(261, 428);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 24);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "obstacle";
+            // 
+            // Floor
+            // 
+            this.Floor.BackColor = System.Drawing.Color.Black;
+            this.Floor.Location = new System.Drawing.Point(-12, 452);
+            this.Floor.Name = "Floor";
+            this.Floor.Size = new System.Drawing.Size(652, 23);
+            this.Floor.TabIndex = 0;
+            this.Floor.TabStop = false;
+            // 
+            // Avatar
+            // 
+            this.Avatar.BackColor = System.Drawing.Color.Transparent;
+            this.Avatar.Location = new System.Drawing.Point(46, 405);
+            this.Avatar.Name = "Avatar";
+            this.Avatar.Size = new System.Drawing.Size(24, 48);
+            this.Avatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Avatar.TabIndex = 1;
+            this.Avatar.TabStop = false;
+            this.Avatar.Click += new System.EventHandler(this.Avatar_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(356, 233);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click_3);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
             this.ClientSize = new System.Drawing.Size(618, 475);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Floor);
@@ -113,10 +124,11 @@
             this.Text = "SL2aGame";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyPressed);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyReleased);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Floor)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Avatar)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,6 +142,7 @@
         private System.Windows.Forms.Timer gameTimer;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
