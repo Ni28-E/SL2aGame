06a0f8253666769ef7f38e9b2e12f477372e8deb
bdbekhof <brian__98@hotmail.nl>
2024-04-12 18:50:08 +0200

    Removed flame obstacle

--- SL2aGame/Form1.Designer.cs
+++ SL2aGame/Form1.Designer.cs
@@ -35,12 +35,10 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.Floor = new System.Windows.Forms.PictureBox();
             this.Avatar = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Floor)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Avatar)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // scoreText
@@ -101,29 +99,19 @@
             this.Avatar.TabIndex = 1;
             this.Avatar.TabStop = false;
             // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::SL2aGame.Properties.Resources.obstacle_fire;
-            this.pictureBox3.Location = new System.Drawing.Point(455, 249);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(162, 204);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox3.TabIndex = 4;
-            this.pictureBox3.TabStop = false;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateGray;
             this.ClientSize = new System.Drawing.Size(618, 475);
-            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Floor);
             this.Controls.Add(this.scoreText);
             this.Controls.Add(this.Avatar);
             this.Name = "Form1";
+            this.Tag = "obstacle";
             this.Text = "SL2aGame";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyPressed);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyReleased);
@@ -131,7 +119,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Floor)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Avatar)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,7 +132,6 @@
         private System.Windows.Forms.Timer gameTimer;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
